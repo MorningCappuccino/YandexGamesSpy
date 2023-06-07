f8b075e77d86dff8c1712803e2220504e54e4cb3
MorningCappuccino <knockknockwakeup@gmail.com>
2023-06-07 18:39:17 +0400

    add lifespan of game

--- src/ygspy.ts
+++ src/ygspy.ts
@@ -56,27 +56,37 @@ export function YandexGamesSpy(config: Config): void {
         return Math.ceil(playersCount / daysPassed);
     }
 
+    function GetDaysPassedBeautify(datePublished: number) {
+        return moment(moment(datePublished), "YYYYMMDD").fromNow();
+    }
+
     function renderGameInfo(el: Element, game: Game) {
         const wrapper = document.createElement("div");
         const playersEl = document.createElement("div");
         const firstPublished = document.createElement("div");
+        const lifeSpan = document.createElement("div");
         const playersCountVSdaysOfPublished = document.createElement("div");
         const firstPublishedMs = game.firstPublished * 1000;
 
         playersEl.classList.add("egs-players-count");
         firstPublished.classList.add("egs-first-published");
+        lifeSpan.classList.add("egs-life-span");
         wrapper.classList.add("egs-wrapper");
 
         const playersVsDays = PlayersVSDaysPublished(game.playersCount, firstPublishedMs);
 
         playersEl.innerHTML = "<span class='egs-label'>Players </span>" + numBeautifier(game.playersCount);
         firstPublished.innerHTML = "<span class='egs-label'>Release </span>" + moment(firstPublishedMs).format("ll");
+        lifeSpan.innerHTML = ` (${GetDaysPassedBeautify(firstPublishedMs)})`;
         playersCountVSdaysOfPublished.innerHTML = "<span class='egs-label'>Players/Days </span>" + playersVsDays;
 
         wrapper.append(playersEl);
         wrapper.append(firstPublished);
+        wrapper.append(lifeSpan);
         wrapper.append(playersCountVSdaysOfPublished);
         el.append(wrapper);
+
+        el.setAttribute("data-egs-added", "");
 
         if (config.colorsBy == ColorsBy.DatePublished) {
             ColorWrapperByPublishedDate(wrapper, firstPublishedMs);
@@ -91,7 +101,7 @@ export function YandexGamesSpy(config: Config): void {
             res.games.map((game) => {
                 // console.log(game.playersCount)
                 document.querySelectorAll(".game-card__game-url").forEach((el) => {
-                    if (+el.getAttribute("href").match(/\d+/)[0] == game.appID) {
+                    if (+el.getAttribute("href").match(/\d+/)[0] == game.appID && !el.hasAttribute("data-egs-added")) {
                         renderGameInfo(el, game);
                     }
                 });
